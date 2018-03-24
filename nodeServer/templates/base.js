dd75ee0e6ac145be842e2ff37842139b59306bf8
Makan Azarshahy <oro1919@gmail.com>
2018-03-24 10:22:25 -0700

    Added server side template functions

---
+++ nodeServer/templates/base.js
@@ -0,0 +1,42 @@
+const createBaseHtml = (title, body, scripts) => `
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>${title}</title>
+</head>
+<body>
+  <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+  <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+  <div id="main">
+    <h1>THIS</h1>
+    <h1>IS</h1>
+    <h1>SERVER-SIDE RENDERING!</h1>
+    ${body}
+    ${scripts}
+  </div>
+</body>
+</html>
+`;
+
+const createAppBody = (about, reservation, menu, review) => `
+  <div id="about">${about}</div>
+  <div id="reservations">${reservation}</div>
+  <div id="menu">${menu}</div>
+  <div id="reviews">${review}</div>
+`;
+
+const createHydrated = (about, reservation, menu, review) => `
+  <script src='http://localhost:3009/bundle.js'></script>
+  <script>
+ReactDOM.hydrate(
+  React.createElement(${menu}, { name: "voluptatem9999975"}),
+  document.getElementById('menu')
+);
+  </script>
+`;
+
+module.exports = {
+  createBaseHtml,
+  createAppBody,
+  createHydrated,
+};
