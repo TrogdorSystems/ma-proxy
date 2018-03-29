d45d1a9efb96d1d73806d013d2034df5c7b73e06
Makan Azarshahy <oro1919@gmail.com>
2018-03-29 10:35:48 -0700

    Changed names to work with naming convention

--- nodeServer/bundlePaths/servicePaths.js
+++ nodeServer/bundlePaths/servicePaths.js
@@ -1,14 +1,14 @@
 const servicePaths = {
-  // about: '',
-  reservations: 'http://ec2-54-219-137-44.us-west-1.compute.amazonaws.com',
-  menu: 'http://ec2-54-67-41-26.us-west-1.compute.amazonaws.com',
+  // SummaryView: '',
+  // ReservationView: 'http://ec2-54-219-137-44.us-west-1.compute.amazonaws.com',
+  MenuView: 'http://ec2-54-67-41-26.us-west-1.compute.amazonaws.com',
   // reviews: '',
 };
 
 const serviceList = [
-  // 'about',
-  'reservations',
-  'menu',
+  // 'SummaryView',
+  // 'ReservationView',
+  'MenuView',
   // 'reviews',
 ];
 
