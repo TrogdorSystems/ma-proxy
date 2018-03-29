a5ee3734d7ddb46a61262821da152b391ef7454e
Makan Azarshahy <oro1919@gmail.com>
2018-03-28 20:26:27 -0700

    Refactored code to work with ec2 instances

--- nodeServer/redisClient.js
+++ nodeServer/redisClient.js
@@ -2,7 +2,7 @@ const redis = require('redis');
 
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
-const redisClient = redis.createClient('redis://redis:6379');
+const redisClient = redis.createClient('redis://ec2-13-57-11-252.us-west-1.compute.amazonaws.com:6379');
 
 redisClient.on('connect', () => console.log('CONNECTED TO REDIS'));
 
